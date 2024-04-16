3c3e07f5b9c882cd33d5cc8c23cdec673062921b
alexandru_lupascu <alex@omniatech.io>
2024-04-16 19:55:20 +0300

    STA-06: Added storage gap in the base contract.

--- src/Staking.sol
+++ src/Staking.sol
@@ -571,6 +571,13 @@ contract Staking is
     mapping(uint256 => StakingUtils.Stake) private _stakes;
     mapping(address => uint256[]) private _stakeIds;
 
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[50] private __gap;
+
     error ExistingMeasurement(bytes32 nodeId);
     error NothingStaked(bytes32 nodeId);
 
